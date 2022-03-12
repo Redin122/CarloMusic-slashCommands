47f29fec6db6d0a910a72f0e18593a190bb4d9fb
Redin122 <100592795+Redin122@users.noreply.github.com>
2022-03-12 15:34:23 +0300

    Update config.js

--- src/config.js
+++ src/config.js
@@ -1,13 +1,13 @@
 module.exports = {
-    token: "",
-    prefix: "",
-    ownerID: "",
+    token: "OTUyMTc3NDU2MTIxNTE2MTQz.YiyOXA.ubnb180gkEcgpm6UgHR2vfIFsto",
+    prefix: "_",
+    ownerID: "905863161457025055",
     SpotifyID: "",
     SpotifySecret: "",
-    mongourl: "",
-    embedColor: "",
-    logs: "",
-
+    mongourl: "if you need a mongodb, join our community and we happy to help you:
+https://discord.gg/gGzf9t3d6G",
+    embedColor: "RANDOM",
+    logs: "952182209501077536",//Logs Channel join join w left
   nodes: {
      
       host: "disbotlistlavalink.ml",
